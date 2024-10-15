2e7109cf1aa2b0a392187c87c3d7c7254b0911a2
REIGaruki <gyakujo@mail.ru>
2024-10-15 22:53:46 +0300

    Imported Recommendation class into RecommendationRuleSet

--- src/main/java/com/skypro/teamwork/rulesets/RecommendationRuleSet.java
+++ src/main/java/com/skypro/teamwork/rulesets/RecommendationRuleSet.java
@@ -1,8 +1,11 @@
 package com.skypro.teamwork.rulesets;
 
+import com.skypro.teamwork.object.Recommendation;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RecommendationRuleSet {
     //интерфейс получает id пользователя и возвращает объект рекомендации или null
-    public Optional<Recommendation> checkRuleMatching(UUID uesrID);
+    public Optional<Recommendation> checkRuleMatching(UUID userID);
 }
