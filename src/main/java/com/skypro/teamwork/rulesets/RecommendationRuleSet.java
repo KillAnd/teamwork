3037242cb4d2f88dbde95c8435e22e21d323f130
REIGaruki <gyakujo@mail.ru>
2024-10-15 21:47:02 +0300

    Contract for future implementations

--- src/main/java/com/skypro/teamwork/rulesets/RecommendationRuleSet.java
+++ src/main/java/com/skypro/teamwork/rulesets/RecommendationRuleSet.java
@@ -1,4 +1,8 @@
 package com.skypro.teamwork.rulesets;
 
+import java.util.UUID;
+
 public interface RecommendationRuleSet {
+    //интерфейс получает id пользователя и возвращает объект рекомендации или null
+    public Optional<Recommendation> recommend(UUID uesrID);
 }
