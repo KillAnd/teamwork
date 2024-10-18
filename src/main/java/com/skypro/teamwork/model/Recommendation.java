af27499e2dac452702e793c4876bc3b412a12c3f
HomeMedia <taknado13@yandex.ru>
2024-10-18 15:28:13 +0700

    добавил методы репозитория для запросов в бд создал объект

---
+++ src/main/java/com/skypro/teamwork/model/Recommendation.java
@@ -0,0 +1,64 @@
+package com.skypro.teamwork.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Recommendation {
+    UUID id;
+    String name;
+    String text;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Recommendation(UUID id, String name, String text) {
+        this.id = id;
+        this.name = name;
+        this.text = text;
+    }
+    public Recommendation() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recommendation that = (Recommendation) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, text);
+    }
+
+    @Override
+    public String toString() {
+        return "Recommendation{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/skypro/teamwork/repository/RecommendationsRepository.java
+++ src/main/java/com/skypro/teamwork/repository/RecommendationsRepository.java
@@ -1,9 +1,11 @@
 package com.skypro.teamwork.repository;
 
+import com.skypro.teamwork.model.User;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -15,12 +17,61 @@ public class RecommendationsRepository {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public int getRandomTransactionAmount(UUID user){
+    public int getRandomTransactionAmount(UUID user) {
         Integer result = jdbcTemplate.queryForObject(
                 "SELECT amount FROM transactions t WHERE t.user_id = ? LIMIT 1",
                 Integer.class,
                 user);
         return result != null ? result : 0;
     }
+
+    public List<UUID> readyInvest5000() {
+        List<UUID> result = jdbcTemplate.queryForObject(
+                "SELECT t.user_id" +
+                        "FROM transactions t" +
+                        "JOIN products p ON t.product_id = p.id" +
+                        "WHERE p.TYPE NOT IN ('INVEST')" +
+                        "GROUP BY t.USER_ID" +
+                        "HAVING" +
+                        "SUM(CASE WHEN p.type = 'INVEST' THEN 1 ELSE 0 END) = 0 AND" +
+                        "SUM(CASE WHEN p.type = 'DEBIT' THEN 1 ELSE 0 END) > 0 AND" +
+                        "SUM(CASE WHEN p.type = 'SAVING' THEN t.amount ELSE 0 END) > 1000",
+                List.class);
+        return result;
+    }
+
+    public List<UUID> readyTopSaving() {
+        List<UUID> result = jdbcTemplate.queryForObject(
+                "SELECT t.user_id" +
+                        "FROM transactions t" +
+                        "JOIN products p ON t.product_id = p.id" +
+                        "GROUP BY t.USER_ID" +
+                        "HAVING" +
+                        "SUM(CASE WHEN p.type = 'DEBIT' THEN t.amount ELSE 0 END) >= 50000" +
+                        "OR SUM(CASE WHEN p.type = 'SAVING' THEN t.amount ELSE 0 END) >= 50000" +
+                        "AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >" +
+                        "SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END)" +
+                        "AND SUM(CASE WHEN p.type = 'DEBIT' THEN t.amount ELSE 0 END) > 0",
+                List.class);
+        return result;
+    }
+
+    public List<UUID> readyCredit() {
+        List<UUID> result = jdbcTemplate.queryForObject(
+                "SELECT t.user_id" +
+                        "FROM transactions t" +
+                        "JOIN products p ON t.product_id = p.id" +
+                        "GROUP BY t.USER_ID" +
+                        "HAVING" +
+                        "SUM(CASE WHEN p.type = 'INVEST' THEN 1 ELSE 0 END) = 0 AND" +
+                        "SUM(CASE WHEN p.type = 'DEBIT' THEN t.amount ELSE 0 END) > 100000" +
+                        "AND SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'DEPOSIT' THEN t.amount ELSE 0 END) >" +
+                        "SUM(CASE WHEN p.type = 'DEBIT' AND t.type = 'WITHDRAW' THEN t.amount ELSE 0 END)",
+                List.class);
+        return result;
+    }
+
+
+
 
 }
