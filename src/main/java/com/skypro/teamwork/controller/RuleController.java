526acc6cf726a8d391717d4d5b6a31917b1dd896
REIGaruki <gyakujo@mail.ru>
2024-10-22 14:09:47 +0300

    Создан POST-запрос

--- src/main/java/com/skypro/teamwork/controller/RuleController.java
+++ src/main/java/com/skypro/teamwork/controller/RuleController.java
@@ -3,10 +3,7 @@ package com.skypro.teamwork.controller;
 import com.skypro.teamwork.model.Recommendation;
 import com.skypro.teamwork.repository.ObjectRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -26,14 +23,32 @@ public class RuleController {
     @GetMapping("/{recommendationId}")
     public ResponseEntity<Map<String, Object>> getRulesOfRecommendation(@PathVariable UUID recommendationId) {
         Recommendation recommendation = repository.findById(recommendationId);
-        List<Rule> rules = recommendation.getRules();
         Map<String, Object> response = new HashMap<>();
         Map<String, Object> data = new HashMap<>();
         data.put("id", UUID.randomUUID());
         data.put("product_name", recommendation.getName());
-        data.put("product_id", recommendationId);
+        data.put("product_id", recommendation.getId());
         data.put("product_text", recommendation.getText());
-        data.put("rule", rules);
+        data.put("rule", recommendation.getRules());
+        response.put("data", data);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> createRulesOfRecommendation(
+            @RequestParam(name = "product_name") String name,
+            @RequestParam(name = "product_id") UUID id,
+            @RequestParam(name = "product_text") String text,
+            @RequestParam(name = "rule") List<Rule> rules
+    ) {
+        Recommendation recommendation = repository.createRecommendation(name, id, text, rules);
+        Map<String, Object> response = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
+        data.put("id", UUID.randomUUID());
+        data.put("product_name", recommendation.getName());
+        data.put("product_id", recommendation.getId());
+        data.put("product_text", recommendation.getText());
+        data.put("rule", recommendation.getRules());
         response.put("data", data);
         return ResponseEntity.ok(response);
     }
