ac6ae7bae62696bb12c32c52fb26737c27a6da0a
REIGaruki <gyakujo@mail.ru>
2024-10-28 14:16:07 +0300

    Созданы массивы аргументов

--- src/main/java/com/skypro/teamwork/repository/ArgumentsRepository.java
+++ src/main/java/com/skypro/teamwork/repository/ArgumentsRepository.java
@@ -2,6 +2,14 @@ package com.skypro.teamwork.repository;
 
 public class ArgumentsRepository {
 
+    private final String[] QUERIES = new String[]{"USER_OF", "ACTIVE_USER_OF", "TRANSACTION_SUM_COMPARE", "TRANSACTION_SUM_COMPARE_DEPOSIT_WITHDRAW"};
+
+    private final String[] PRODUCT_TYPES = new String[] {"DEBIT", "CREDIT", "INVEST", "SAVING"};
+
+    private final String[] TRANSACTION_TYPES = new String[] {"WITHDRAW", "DEPOSIT"};
+
+    private final String[] RELATION_OPERATORS = new String[] {">", "<", ">=", "<=", "=="};
+
 
 
 }
