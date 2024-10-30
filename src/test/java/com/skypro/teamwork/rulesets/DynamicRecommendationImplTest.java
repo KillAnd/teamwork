93d262329053a94ed5b5ebf249bd37dc8b400d00
REIGaruki <gyakujo@mail.ru>
2024-10-30 21:01:15 +0300

    Динамические правила

--- src/test/java/com/skypro/teamwork/rulesets/DynamicRecommendationImplTest.java
+++ src/test/java/com/skypro/teamwork/rulesets/DynamicRecommendationImplTest.java
@@ -9,14 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.CacheManager;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
