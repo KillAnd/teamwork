d221578b2be43c018a73621990e243f43c42b9e4
REIGaruki <gyakujo@mail.ru>
2024-11-01 20:30:51 +0300

    Скорректирована миграция

--- src/main/resources/liquibase/scripts/tableRecommendation.sql
+++ src/main/resources/liquibase/scripts/tableRecommendation.sql
@@ -3,9 +3,9 @@
 -- changeset TeamWorker:1
 CREATE TABLE IF NOT EXISTS recommendation (
     id UUID PRIMARY KEY,
-    product_name VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     product_id UUID NOT NULL,
-    product_text TEXT NOT NULL
+    text TEXT NOT NULL
 );
 
 -- changeset TeamWorker:2
@@ -14,14 +14,14 @@ CREATE TABLE IF NOT EXISTS rule (
     query VARCHAR(255) NOT NULL,
     arguments VARCHAR(255)[] NOT NULL,
     negate BOOLEAN NOT NULL,
-    recommendation_id UUID,
+    recommendation_id UUID NOT NULL,
     FOREIGN KEY (recommendation_id) REFERENCES recommendation(id) ON DELETE CASCADE
 );
 
 -- changeset TeamWorker:3
 CREATE TABLE IF NOT EXISTS stat (
     id UUID PRIMARY KEY,
-    counter int NOT NULL DEFAULT 0,
+    counter INT NOT NULL DEFAULT 0,
     FOREIGN KEY (id) REFERENCES recommendation(id) ON DELETE CASCADE
 );
 
