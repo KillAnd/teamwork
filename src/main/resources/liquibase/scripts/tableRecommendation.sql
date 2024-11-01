b5aa164653fb0ec44d8bc4bca374b933ebf2b63e
REIGaruki <gyakujo@mail.ru>
2024-11-01 20:23:10 +0300

    Создана миграция для добавления таблицы

--- src/main/resources/liquibase/scripts/tableRecommendation.sql
+++ src/main/resources/liquibase/scripts/tableRecommendation.sql
@@ -1,7 +1,7 @@
 -- liquibase/scripts/tableRecommendation.sql
 
 -- changeset TeamWorker:1
-CREATE TABLE IF NOT EXISTS recommendations (
+CREATE TABLE IF NOT EXISTS recommendation (
     id UUID PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
     product_id UUID NOT NULL,
@@ -9,12 +9,20 @@ CREATE TABLE IF NOT EXISTS recommendations (
 );
 
 -- changeset TeamWorker:2
-CREATE TABLE IF NOT EXISTS rules (
+CREATE TABLE IF NOT EXISTS rule (
     id UUID PRIMARY KEY,
     query VARCHAR(255) NOT NULL,
     arguments VARCHAR(255)[] NOT NULL,
     negate BOOLEAN NOT NULL,
-    FOREIGN KEY (id) REFERENCES recommendations(id) ON DELETE CASCADE
+    recommendation_id UUID,
+    FOREIGN KEY (recommendation_id) REFERENCES recommendation(id) ON DELETE CASCADE
+);
+
+-- changeset TeamWorker:3
+CREATE TABLE IF NOT EXISTS stat (
+    id UUID PRIMARY KEY,
+    counter int NOT NULL DEFAULT 0,
+    FOREIGN KEY (id) REFERENCES recommendation(id) ON DELETE CASCADE
 );
 
 
